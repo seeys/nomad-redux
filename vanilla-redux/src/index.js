cf6ede3e9dd41dec4f41ce7266a340506ec1b3cb
siha <es2008e@naver.com>
2022-11-30 21:06:03 +0900

    Feat : 1.4

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,13 +4,17 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -19,10 +23,10 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 add.addEventListener("click", handleAdd);
 
